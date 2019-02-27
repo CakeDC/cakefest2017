d7e6aa1e3a89bb9694d18fd98cc85b93f78b2293
Jorge González <steinkel@gmail.com>
2019-02-27 22:18:53 +0000

    refs #unit-test-code add simple test

---
+++ tests/TestCase/Utils/FormatterTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\Test\TestCase\Utils;
+
+use App\Utils\Formatter;
+use Cake\TestSuite\TestCase;
+
+/**
+ * @property Formatter Formatter
+ */
+class FormatterTest extends TestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->Formatter = new Formatter();
+    }
+
+    public function tearDown()
+    {
+        unset($this->Formatter);
+        parent::tearDown();
+    }
+
+    public function testFormatStatPercentage()
+    {
+        $expected = '50%';
+        $result = $this->Formatter->formatStatPercentage(1,1);
+        $this->assertSame($expected, $result);
+    }
+
+    public function testFormatStatPercentageShouldReturnPlayMoreGames()
+    {
+        $expected = 'Play more games!';
+        $result = $this->Formatter->formatStatPercentage(0,1);
+        $this->assertSame($expected, $result);
+    }
+}
