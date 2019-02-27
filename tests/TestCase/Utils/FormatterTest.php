f26626ecc9a176250dd313ac33d9ca24b6c26cd7
Jorge González <steinkel@gmail.com>
2019-02-27 22:25:53 +0000

    refs #unit-test-code using data provider

--- tests/TestCase/Utils/FormatterTest.php
+++ tests/TestCase/Utils/FormatterTest.php
@@ -9,29 +9,57 @@ use Cake\TestSuite\TestCase;
  */
 class FormatterTest extends TestCase
 {
-    public function setUp()
+    public function setUp() : void
     {
         parent::setUp();
         $this->Formatter = new Formatter();
     }
 
-    public function tearDown()
+    public function tearDown() : void
     {
         unset($this->Formatter);
         parent::tearDown();
     }
 
-    public function testFormatStatPercentage()
+    public function testFormatStatPercentage() : void
     {
         $expected = '50%';
         $result = $this->Formatter->formatStatPercentage(1, 1);
         $this->assertSame($expected, $result);
     }
 
-    public function testFormatStatPercentageShouldReturnPlayMoreGames()
+    public function testFormatStatPercentageShouldReturnPlayMoreGames() : void
     {
         $expected = 'Play more games!';
         $result = $this->Formatter->formatStatPercentage(0, 1);
         $this->assertSame($expected, $result);
+    }
+
+    /**
+     * @dataProvider formatStatPercentageViaProvider
+     * @param $won
+     * @param $lost
+     * @param $expected
+     */
+    public function testFormatStatPercentageViaProvider($won, $lost, $expected) : void
+    {
+        $result = $this->Formatter->formatStatPercentage($won, $lost);
+        $this->assertSame($expected, $result);
+    }
+
+    /**
+     * Data provider for common cases
+     *
+     * @return array
+     */
+    public function formatStatPercentageViaProvider() : array
+    {
+        return [
+            [0,0,'Play more games!'],
+            [0,1,'Play more games!'],
+            [0,2,'Play more games!'],
+            [1,0,'100%'],
+            [2,0,'100%'],
+        ];
     }
 }
